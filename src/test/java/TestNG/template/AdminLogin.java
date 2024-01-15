978e53658a6dc327854d7ee0b2accd3024d43970
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2024-01-15 12:02:49 +0700

    TEMPLATE: ADD AdminLogin.java to be used to instant login

---
+++ src/test/java/TestNG/template/AdminLogin.java
@@ -0,0 +1,28 @@
+package TestNG.template;
+
+import com.juaracoding.drivers.DriverSingleton;
+import com.juaracoding.pages.GlobalElementPage;
+import com.juaracoding.pages.admin.AdminLoginPage;
+import com.juaracoding.utils.Constant;
+import org.openqa.selenium.WebDriver;
+
+public class AdminLogin {
+    final String LOGIN_URL = Constant.ADMIN_URL;
+    private GlobalElementPage globalElementPage;
+    private AdminLoginPage adminLoginPage;
+    private WebDriver driver;
+
+    public AdminLogin(GlobalElementPage globalElementPage, WebDriver driver) {
+        this.globalElementPage = globalElementPage;
+        this.driver = driver;
+        this.adminLoginPage = new AdminLoginPage();
+    }
+
+    public void loginUser() {
+        driver.get(LOGIN_URL);
+        globalElementPage.setEmail("admin@hadir.com");
+        globalElementPage.setPassword("admin@hadir");
+        adminLoginPage.clickAdminLoginButton();
+        DriverSingleton.delay(3);
+    }
+}
